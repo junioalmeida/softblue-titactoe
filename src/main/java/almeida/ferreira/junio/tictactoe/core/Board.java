69a326e2a870fe4924aeaf447fae95324932a376
Junio Almeida <junioferreiradealmeida2001@gmail.com>
2020-06-24 21:08:25 -0300

    Implementacao validacao jogador ganhador

--- src/main/java/almeida/ferreira/junio/tictactoe/core/Board.java
+++ src/main/java/almeida/ferreira/junio/tictactoe/core/Board.java
@@ -32,9 +32,9 @@ public class Board {
 					UI.printTextWithoutNewLine(" | ");
 				}
 			}
-			
+
 			UI.printNewLine();
-			
+
 			if (i + 1 < Constants.BOARD_SIZE) {
 				UI.printText("---------");
 			}
@@ -53,16 +53,88 @@ public class Board {
 		return true;
 	}
 
-	public void play(Player player, Move move) {
-		
+	public boolean play(Player player, Move move) {
+
 		int i = move.getI();
 		int j = move.getJ();
 		
-		//TODO Desenvolver checagem de vencedor
-		//TODO Desenvolver checagem de termino do jogo
-		//TODO Desenvolver verificar se a jogada e valida
-		
+		// TODO Desenvolver checagem de termino do jogo
+		// TODO Desenvolver verificar se a jogada e valida
+
 		matrix[i][j] = player.getSymbol();
+		
+		return checkRows(player) || checkColumns(player) || checkDiagonal1(player) || checkDiagonal2(player);
 	}
 
+	private boolean checkRows(Player player) {
+
+		for (int i = 0; i < Constants.BOARD_SIZE; i++) {
+			if (checkRow(i, player))
+				return true;
+		}
+
+		return false;
+	}
+
+	private boolean checkRow(int i, Player player) {
+
+		char symbol = player.getSymbol();
+
+		for (int j = 0; j < Constants.BOARD_SIZE; j++) {
+			if (symbol != matrix[i][j])
+				return false;
+		}
+
+		return true;
+	}
+
+	private boolean checkColumns(Player player) {
+
+		for (int j = 0; j < Constants.BOARD_SIZE; j++) {
+			if (checkColumn(j, player))
+				return true;
+		}
+
+		return false;
+	}
+
+	private boolean checkColumn(int j, Player player) {
+
+		char symbol = player.getSymbol();
+
+		for (int i = 0; i < Constants.BOARD_SIZE; i++) {
+			if (symbol != matrix[i][j])
+				return false;
+		}
+
+		return true;
+	}
+
+	private boolean checkDiagonal1(Player player) {
+
+		char symbol = player.getSymbol();
+
+		for (int i = 0; i < Constants.BOARD_SIZE; i++) {
+			if(symbol != matrix[i][i]) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+	private boolean checkDiagonal2(Player player) {
+
+		char symbol = player.getSymbol();
+		int lastLine = Constants.BOARD_SIZE - 1;
+		
+		for (int i = lastLine, j = 0; i < lastLine; i++, j--) {
+			if(symbol != matrix[i][j]) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
 }
